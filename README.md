cd714419b6d891776da227cc36064b206b6e2df1
Filipe Madureira <contact@filipemadureira.com>
2019-01-22 00:03:16 +0100

    Created basic webpack config, imported the output main-bundle.js file in the index.html file.

---
+++ README.md
@@ -0,0 +1,8 @@
+# react-webpack
+
+I made this project to learn to use webpack and react from scratch.
+
+1. Build a custom development environment from scratch using webpack
+2. Hook react.js to webpack
+3. Learn react.js
+
---
+++ config/webpack.dev.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: { // File in entrata
+        main: ['./src/main.js']
+    },
+    mode: 'development',
+    output: { // File in uscita, usa p.es.'main' al posto di [name], preso dalle entry{}
+        filename: '[name]-bundle.js',
+        path: path.resolve(__dirname,'../dist'), // Dove mettere i file in uscita
+        publicPath: '/'// dove mettere i file output, partendo dalla path specifica sopra
+    },
+    devServer: {
+        contentBase: "dist" 
+    }
+}
