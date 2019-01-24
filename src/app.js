78acd70d501605557c3af46e03ef94185c51e9b4
Filipe Madureira <contact@filipemadureira.com>
2019-01-25 00:34:50 +0100

    Added React hot-reloading, Added two new components: counter and list.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from  './root';
+
+// Adds the .hot property to this js module
 import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-    <h1>react-webpack</h1>,
-    document.getElementById('react-root')
-)
+// Function that can be used to rended any component and still use the hot loader
+function render(Component){
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('react-root')
+    )
+}
+
+// Actually render stuff here
+render(Root);
+// render(List, 'list-component');
+
+// Checks if the module is hot, in case it is it reloads the page
+// without losing all the data saved in the state. The module becomes hot
+// whenever an assets gets edited and recompilation is necessary for hot reloading.
+if(module.hot){
+    module.hot.accept('./root.js', () => {
+        const NewRoot = require('./root.js').default;
+        render(NewRoot);
+    })
+}
---
+++ src/counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <h1>{this.props.number}</h1>
+        );
+    }
+}
