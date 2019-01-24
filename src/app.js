121bde9f2a014b0b720046ac783c1645d756f720
Filipe Madureira <contact@filipemadureira.com>
2019-01-24 16:55:06 +0100

    Hooked React

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+ReactDOM.render(
+    <h1>react-webpack</h1>,
+    document.getElementById('react-root')
+)
