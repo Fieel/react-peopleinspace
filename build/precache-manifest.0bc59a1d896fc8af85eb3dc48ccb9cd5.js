df9f0bbb5bb36ba92c215948bde58b79f3505074
Filipe Madureira <contact@filipemadureira.com>
2019-01-26 18:21:06 +0100

    Added build package in the repo

---
+++ build/precache-manifest.0bc59a1d896fc8af85eb3dc48ccb9cd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "37bc1a4ef3d8bc2bf4f8",
+    "url": "/static/js/main.37bc1a4e.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "37bc1a4ef3d8bc2bf4f8",
+    "url": "/static/css/main.caf27196.chunk.css"
+  },
+  {
+    "revision": "ff29b811c634b6b5deef4e6d136ce65b",
+    "url": "/index.html"
+  }
+];
