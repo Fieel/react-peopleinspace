69624368659e63e232365108385462dbd7370a58
Fieel <contact@filipemadureira.com>
2019-02-03 15:31:15 +0100

    Added express.js server, added scripts to run prod build.. Added config for Heroku.

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+var environment = require('serve-static');
+
+app = express();
+
+console.log(process.env.NODE_ENV);
+
+var morgan = require('morgan')
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+} else {
+    app.use(morgan('short'))
+}
+
+app.use(serveStatic(__dirname + "/build"));
+
+var port = process.env.PORT || 5000;
+process.env.NODE_ENV = 'production';
+
+app.listen(port);
+
+console.log('');
+console.log('**************************************');
+console.log('* Express.js server started          *');
+console.log('* Port: '+port+'                         *');
+console.log('* Listening on http://localhost:'+port+' *');
+console.log('**************************************');
+console.log('');
